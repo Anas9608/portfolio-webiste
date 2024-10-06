49cb487fdb30ab26f4e3276bbc3cba4a9de68db1
Mohd Anas <anas0shah0@gmailcom>
2024-10-06 17:22:48 +0530

    starting on building portfolio website FE

---
+++ src/app/porfolio-website/porfolio-website.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PorfolioWebsiteRoutingModule } from './porfolio-website-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PorfolioWebsiteRoutingModule
+  ]
+})
+export class PorfolioWebsiteModule { }
